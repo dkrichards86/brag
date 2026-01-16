1213a9a79ff1e059242c6127138d23706d9a2702
Keith Richards <dkrichards86@gmail.com>
2026-01-16 15:17:31 -0500

    Fix cyclomatic complexity errors

--- cmd/untag.go
+++ cmd/untag.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -21,43 +20,25 @@ func init() {
 }
 
 func removeTag(cmd *cobra.Command, args []string) {
-	store, err := getStorage()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Parse line number (supports "last" keyword)
-	index, err := parseLineNumber(args[0], store)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Process all tags and ensure they start with #
-	var tags []string
-	for _, arg := range args[1:] {
-		tag := strings.TrimSpace(arg)
-		if !strings.HasPrefix(tag, "#") {
-			tag = "#" + tag
-		}
-		tags = append(tags, tag)
-	}
-
-	wins, err := store.ReadAllWins()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading wins: %v\n", err)
-		os.Exit(1)
-	}
-
-	if index < 0 || index >= len(wins) {
-		fmt.Fprintf(os.Stderr, "Error: line number is out of range (1-%d)\n", len(wins))
-		os.Exit(1)
-	}
+	store, index := mustGetStorageAndIndex(args[0])
+	tags := normalizeTags(args[1:])
+	wins := mustReadWins(store)
+	mustValidateIndex(index, len(wins))
 
 	win := wins[index]
+	removedTags, missingTags := partitionRemovedAndMissingTags(win, tags)
 
-	// Check which tags exist and which don't
+	if len(removedTags) == 0 {
+		displayNoTagsRemovedMessage(missingTags)
+		return
+	}
+
+	newMessage := removeTagsFromMessage(win.Message, removedTags)
+	mustUpdateWin(store, index, newMessage)
+	displayRemoveTagResults(removedTags, missingTags, index)
+}
+
+func partitionRemovedAndMissingTags(win interface{ HasTag(string) bool }, tags []string) ([]string, []string) {
 	var removedTags []string
 	var missingTags []string
 	for _, tag := range tags {
@@ -67,35 +48,29 @@ func removeTag(cmd *cobra.Command, args []string) {
 			missingTags = append(missingTags, tag)
 		}
 	}
+	return removedTags, missingTags
+}
 
-	// If no tags to remove, nothing to do
-	if len(removedTags) == 0 {
-		if len(missingTags) == 1 {
-			fmt.Printf("Tag %s doesn't exist on this win.\n", missingTags[0])
-		} else {
-			fmt.Printf("Tags %s don't exist on this win.\n", strings.Join(missingTags, ", "))
-		}
-		return
+func displayNoTagsRemovedMessage(missingTags []string) {
+	if len(missingTags) == 1 {
+		fmt.Printf("Tag %s doesn't exist on this win.\n", missingTags[0])
+	} else {
+		fmt.Printf("Tags %s don't exist on this win.\n", strings.Join(missingTags, ", "))
 	}
+}
 
-	// Remove the tags from the message
-	newMessage := win.Message
-	for _, tag := range removedTags {
+func removeTagsFromMessage(message string, tags []string) string {
+	newMessage := message
+	for _, tag := range tags {
 		// Remove tag with space before it, or just the tag if no space
 		newMessage = strings.ReplaceAll(newMessage, " "+tag, "")
 		newMessage = strings.ReplaceAll(newMessage, tag, "")
 	}
-
 	// Clean up any double spaces that might result
-	newMessage = strings.Join(strings.Fields(newMessage), " ")
+	return strings.Join(strings.Fields(newMessage), " ")
+}
 
-	// Update the win
-	if err := store.UpdateWin(index, newMessage); err != nil {
-		fmt.Fprintf(os.Stderr, "Error updating win: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Report results
+func displayRemoveTagResults(removedTags, missingTags []string, index int) {
 	if len(removedTags) == 1 {
 		fmt.Printf("Removed tag %s from win #%d\n", removedTags[0], index+1)
 	} else {
